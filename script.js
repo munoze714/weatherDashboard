422515a25a4bb8a6c4d7f944a5f2d29435ffe3e5
Elizabeth Munoz <munoz_714@live.com>
2020-03-24 09:30:55 -0700

    add color background to uv index

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ $(document).ready(function () {
         $.ajax({
             url: apiUrl,
             type: "get",
+
         }).then(function (data) {
             console.log(data)
             $("#displayCity").text(data.name + "(" + date + ")")
@@ -18,9 +19,9 @@ $(document).ready(function () {
             $("#displayHumidity").text("Humidity " + data.main.humidity + "%")
             $("#displayCity").append(DisplayIcon)
             weekDisplay(searchValue)
+
             var lat = data.coord.lat
             var lon = data.coord.lon
-
             uvDisplay(lat, lon)
         })
     }
@@ -31,26 +32,37 @@ $(document).ready(function () {
         $.ajax({
             url: apiUrl,
             method: "get",
+
         }).then(function (data) {
             // console.log(data)
             // console.log(data.value)
             var uvIndex = $("<div>").attr("id", "uvDisplay")
             $(".col2").append(uvIndex)
-            $("#uvDisplay").text("UV Index: " + data.value)
+            $("#uvDisplay").text("UV Index: ")
             uvIndex.append($("#uvDisplay"))
 
+            var favorable = $("<button>").text(data.value).attr("style", "background-color: green")
+            var moderate = $("<button>").text(data.value).attr("style", "background-color: orange")
+            var severe = $("<button>").text(data.value).attr("style", "background-color: red")
+
+            if (data.value <= 2) {
+                $("#uvDisplay").append(favorable)
+            } else if (data.value <= 5) {
+                $("#uvDisplay").append(moderate)
+            } else {
+                $("#uvDisplay").append(severe)
+            }
         })
     }
 
-    // https://cors-anywhere.herokuapp.com/http://api.openweathermap.org/data/2.5/weather
     function weekDisplay(weekValue) {
         var apiUrl = "https://cors-anywhere.herokuapp.com/http://api.openweathermap.org/data/2.5/forecast?q=" + weekValue + "&appid=b6c076c64edd480f39bbc31ea63b4a0f&units=imperial"
         $.ajax({
             url: apiUrl,
             type: "get",
+
         }).then(function (data) {
             // console.log(data)
-
             var forecastArray = [];
             for (var i = 0; i < data.list.length; i++) {
                 //console.log("each single thing", data.list[i].dt_txt.split(' ')[1])
@@ -58,15 +70,16 @@ $(document).ready(function () {
                     forecastArray.push(data.list[i])
                 }
             }
-            // console.log('its all said and done!!', forecastArray);
+            // console.log(forecastArray);
             for (var i = 0; i < forecastArray.length; i++) {
                 var icon = forecastArray[i].weather[0].icon
                 // console.log(icon)
+                // "http://openweathermap.org/img/wn/10d@2x.png>" url for icon
+
                 var weatherIcon = $("<img>").attr("src", "http://openweathermap.org/img/wn/" + icon + "@2x.png")
                 var cardCol = $("<div>").attr("class", "col-2")
                 var cardDisplay = $("<div>").attr("class", "card")
                 var text = $('<p>')
-                // "http://openweathermap.org/img/wn/10d@2x.png>" url for icon
                 text.text(forecastArray[i].dt_txt.split(" ")[0] + " Temp: " + forecastArray[i].main.temp +
                     " Humidity: " + forecastArray[i].main.humidity)
                 // console.log(weatherIcon)
